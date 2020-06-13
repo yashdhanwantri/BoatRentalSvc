cbf2f6f8c4877dcfffd0c23bdb92dab19bb4c26e
Yash Dhanwantri <yashdhanwantri@gmail.com>
2020-06-13 21:19:48 +0530

    Precision Added to HourlyRate

---
+++ Migrations/20200613154901_PrecisionAddedToHourlyRate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BoatRentalSvc.Migrations
+{
+    public partial class PrecisionAddedToHourlyRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HourlyRate",
+                table: "Boats",
+                type: "decimal(10,4)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "HourlyRate",
+                table: "Boats",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,4)");
+        }
+    }
+}
